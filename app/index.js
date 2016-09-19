47867c47e0c35850b13e3d78c0f371ed74b2da15
respectYourNeighbour <philip_luca@yahoo.com>
2016-09-19 11:55:12 +0300

    Step 2
    Nested Components and Props In React

--- app/index.js
+++ app/index.js
@@ -5,11 +5,47 @@ var app = document.getElementById('app');
 var HelloWorld = React.createClass({
   render: function(){
     return (
-      <div>Hello World!</div>
+      <div>Hello, {this.props.name} !</div>
       )
   }
-})
+});
+
+var FriendsContainer = React.createClass({
+  render: function() {
+    var name = this.props.name;
+    var friends = ['Mike','Tyson'];
+
+    return (
+        <div>
+          <h3>Name: {name}</h3>
+          <ShowList names={friends} />
+        </div>
+      )
+  }
+});
+
+var ShowList = React.createClass({
+  render: function(){
+    var listItems = this.props.names.map(function(friend, i){
+      return <li key={i}>{friend} {i}</li>
+    });
+    return (
+      <div>
+        <h3>Friends </h3>
+        <ul>
+          {listItems}
+        </ul>
+      </div>
+    )
+
+  }
+});
 
 ReactDOM.render(
-    <HelloWorld/>,app
+  <HelloWorld name="Filip"/>,app
+);
+
+
+ReactDOM.render(
+  <FriendsContainer name="John Doe"/>, document.getElementById('friends')
 );
