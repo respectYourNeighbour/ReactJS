6f9af752c2477f794de2bc2a5fe25dfa9c0e413d
respectYourNeighbour <philip_luca@yahoo.com>
2016-09-26 14:47:03 +0300

    Step 8
    Axios, Promises, and the Github API

--- app/components/ConfirmBattle.js
+++ app/components/ConfirmBattle.js
@@ -1,9 +1,13 @@
 var React = require('react');
 
+function puke(object) {
+    return <pre>{JSON.stringify(object, null, ' ')}</pre>
+}
+
 function ConfirmBattle(props) {
     return props.isLoading === true
         ? <p>Loading</p>
-        : <p> Confirm BATTLE</p>
+        : <p> Confirm BATTLE: {puke(props)}</p>
 }
 
 module.exports = ConfirmBattle;
--- app/containers/ConfirmBattleContainer.js
+++ app/containers/ConfirmBattleContainer.js
@@ -1,5 +1,6 @@
 var React = require('react');
 var ConfirmBattle = require('../components/ConfirmBattle');
+var githubHelpers = require('../utils/githubHelpers');
 
 var ConfirmBattleContainer = React.createClass({
     contextTypes: {
@@ -18,6 +19,14 @@ var ConfirmBattleContainer = React.createClass({
     componentDidMount: function() {
         console.log("componentDidMount");
         var query = this.props.location.query;
+        githubHelpers.getPlayersInfo([query.playerOne, query.playerTwo])
+        .then(function(players) {
+            console.log('PLAYERS', players);
+            this.setState({
+                isLoading: false,
+                playersInfo: [players[0], players[1]]
+            })
+        }.bind(this))
         console.log('QUERY', query);
     },
     componentWillReceiveProps: function() {
