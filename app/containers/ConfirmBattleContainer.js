764fc4e75ff06482c81eaa8c0a346775ed52b70d
respectYourNeighbour <philip_luca@yahoo.com>
2016-09-28 14:51:26 +0300

    Step 9
    The results page.

--- app/containers/ConfirmBattleContainer.js
+++ app/containers/ConfirmBattleContainer.js
@@ -10,7 +10,7 @@ var ConfirmBattleContainer = React.createClass({
         console.log("getInitialState");
         return {
             isLoading: true,
-            playerInfo: []
+            playersInfo: []
         }
     },
     componentWillMount: function() {
@@ -35,10 +35,19 @@ var ConfirmBattleContainer = React.createClass({
     componentWillUnmount: function() {
         console.log("componentWillUnmount");
     },
+    handleInitiateBattle: function() {
+        this.context.router.push({
+            pathname: '/results',
+            state: {
+                playersInfo: this.state.playersInfo
+            }
+        })
+    },
     render: function() {
         return (
             <ConfirmBattle 
                 isLoading={this.state.isLoading} 
+                onInitiateBattle={this.handleInitiateBattle}
                 playersInfo={this.state.playersInfo}
                 />
         );
